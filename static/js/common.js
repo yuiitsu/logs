786adb8c98c98a7f416d8e1d8782b72f9057fb5c
onlyfu <onlyfu666@gmail.com>
2015-07-10 15:53:56 +0800

    update

--- static/js/common.js
+++ static/js/common.js
@@ -1,18 +1,20 @@
 
+// 设置html字体大小，做为基数
+var sizeHtml = function(){
+	
+	var run = function(){
+		var floSize = $(window).width()/16;
+		floSize = floSize > 45 ? 45 : floSize;
+		$('html').css('font-size', floSize + 'px');
+	}();
+	
+	// 屏幕重置
+	$(window).resize(function(){
+		run();
+	});
+}();
+
 $(function(){
 
-	// 设置html字体大小，做为基数
-	var sizeHtml = function(){
-		
-		var run = function(){
-			var floSize = $(window).width()/16;
-			floSize = floSize > 45 ? 45 : floSize;
-			$('html').css('font-size', floSize + 'px');
-		}();
-		
-		// 屏幕重置
-		$(window).resize(function(){
-			run();
-		});
-	}();
+	
 })
